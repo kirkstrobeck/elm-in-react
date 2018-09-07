74aeea49a7724100e59df01d33cad27c506fb789
Kirk Strobeck <kirk@strobeck.com>
2018-09-07 05:50:20 -0700

    init

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const exclude = [/elm-stuff/, /node_modules/];
+
+module.exports = {
+  mode: 'development',
+
+  entry: './lib/index.jsx',
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.elm$/,
+        exclude,
+        loader: 'elm-webpack-loader'
+      }
+    ],
+
+    noParse: /\.elm$/
+  },
+
+  output: {
+    filename: 'bundle.js'
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.elm']
+  },
+}
